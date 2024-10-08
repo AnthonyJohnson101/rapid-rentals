75c5aa6b885ce595f197551fd8e914f1bf5d62de
AnthonyJohnson101 <tonybologna123@outlook.com>
2024-10-07 23:00:03 -0400

    mongodb adjustment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://tonyj101:tnine845446J!@msu.a8hmq7j.mongodb.net/?retryWrites=true&w=majority&appName=MSU', {
+mongoose.connect('mongodb+srv://tonyj101:gNvS5McBedXxmpBx@msu.a8hmq7j.mongodb.net/?retryWrites=true&w=majority&appName=MSU', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
