cad2fde27256ae92a0ba6adb01dcbe502866034a
AnthonyJohnson101 <tonybologna123@outlook.com>
2024-10-08 17:01:08 -0400

    error logging for mongodb connection and removing depreciated code

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://tonyj101:gNvS5McBedXxmpBx@msu.a8hmq7j.mongodb.net/?retryWrites=true&w=majority&appName=MSU', {
+mongoose.connect('mongodb+srv://tonyj101:gNvS5McBedXxmpBx@msu.a8hmq7j.mongodb.net/rapid-rentals?retryWrites=true&w=majority', {
+/* DEPRECIATED  
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true, */
+})
+.then(() => {
+  console.log('MongoDB connected successfully');
+})
+.catch((err) => {
+  console.error('MongoDB connection error:', err);
 });
 
 // Export connection
